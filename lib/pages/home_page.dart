730d5b8c1452100e1fc76bff7019eba905d76cc3
kishor <kishormali430@gmail.com>
2024-11-25 15:24:56 +0530

    day 3 completed too

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final int days = 30;
+  final int days = 350;
   final String name = "codepur";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page ttt"),
+        title: Text("Catalog App"),
         backgroundColor: Colors.blue,
       ),
       body: Center(
